1569218af29cbcdd6b0e0f4e2abf52411167f3aa
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2023-03-05 20:48:12 -0300

    Adicionados primeiros exercicios

---
+++ AreaDeExercicios/Exercicios/AdivinheONumero/exerciciosCondicionais.js
@@ -0,0 +1,30 @@
+function demonstracao(){
+    var condicao = true;
+    
+    if(condicao){
+
+
+    }
+    
+
+}
+
+// Escreva uma função que recebe 2 valores, escreve o maior deles
+function exMaiorOuMenor(numero1, numero2) {
+    
+
+
+}
+
+// Escrever uma função que recebe o ano de nascimento de uma pessoa e mostrar uma mensagem
+// se ela pode votar este ano ou não. Assuma que a pessoa já fez aniversário este ano
+function exElegivelParaVotar(anoNascimento) {
+    
+}
+
+// Escreve uma função que receba um número de maçãs e retorne o preço total da compra.
+// As maçãs custam R$0,30 cada, com uma promoção se forem compradas pelo menos 12, cada
+// maçã sai pelo preço de R$0.25
+function exVendaDeMacas(quantidade) {
+    
+}
